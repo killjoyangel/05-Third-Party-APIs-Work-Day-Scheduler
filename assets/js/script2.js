ab2531e1706d50222991cdcf10e643e34b1b10b6
Ariel Marchand <marchand.aa@gmail.com>
2021-03-20 18:47:35 -0600

    adding comments

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,6 +3,7 @@ var currentDay = $("#currentDay");
 var saveBtn =$(".saveBtn");
 var timeBlock =$('.time-block')
 var timeItIsRightNow = today.hours()
+var description = $(".description")
 
 function displayTime() {
     var rightNow = today.format('MMM DD, YYYY');
@@ -20,14 +21,22 @@ function updateHours() {
         } else {
         $(this).addClass('future');
     }
+    var id = parseInt($(this).attr('id'))
+        var task = localStorage.getItem(id)
+        if (task != 'null') {
+            $(this).find(description).val(task)
+        }
+
     })
     }
 
-
+    updateHours()
     $('.saveBtn').on('click', function(event) {
         var value = $(this).siblings('.description').val();
         var time = $(this).parent().attr('id');
         console.log(value);
         console.log(time);
+
+        localStorage.getItem(time, value);
         localStorage.setItem(time, value); 
     })
